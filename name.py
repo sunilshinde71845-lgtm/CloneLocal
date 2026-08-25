c105a75bfb3513340623b141abb7018bfe2bcc40
sunilshinde71845-lgtm <sunilshinde71845@gmail.com>
2026-08-25 11:37:15 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,6 +1,8 @@
 name=input("Enter your name:")
 surname=input("Enter your surname:")
 contact=input("Enter your contact number:")
+city=input("Enter your city:")
 
 print("your fullname is:",name+""+surname)
 print("your contact is:",contact)
+print("your city is:",city)
