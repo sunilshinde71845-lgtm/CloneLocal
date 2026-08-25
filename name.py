310f49ced61ffaf4e392f6cdf802e2166f0514fa
suprita <supritauppar17@gmail.com>
2026-08-25 11:35:00 +0530

    saving the file

--- name.py
+++ name.py
@@ -1,4 +1,6 @@
 name=input("Enter your name:")
 surname=input("Enter your surname:")
+contact=input("Enter your contact number:")
 
 print("your fullname is:",name+""+surname)
+print("your contact is:",contact)
